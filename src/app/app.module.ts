8bb862c7531fa3d7e4fe7d2a5268450d1ac3f9bc
nadav <nadav.siv@gmail.com>
2021-04-17 13:43:52 +0300

    added routing/notes list/topnav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
+import { TopNavComponent } from './topNav/top-nav/top-nav.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './modules/material.module';
+import { NotesListComponent } from './notes/notes-list/notes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      TopNavComponent,
+      NotesListComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotesListComponent } from '../notes/notes-list/notes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'notes', pathMatch: 'full' },
+  { path: 'notes', component: NotesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
